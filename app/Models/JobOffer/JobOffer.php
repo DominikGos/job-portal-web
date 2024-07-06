1efc11d947e756b781aeb6251acaa07c7613a72e
DominikGos <dominikgos3@gmail.com>
2024-07-06 22:08:23 +0200

    JP-14 Added joboffer filters, updated validation rules inside salary resource and joboffer

--- app/Models/JobOffer/JobOffer.php
+++ app/Models/JobOffer/JobOffer.php
@@ -7,6 +7,7 @@ use App\Enums\JobOffer\WorkSchedules;
 use App\Enums\JobOffer\WorkTypes;
 use App\Models\Skill;
 use App\Models\User\User;
+use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -54,5 +55,13 @@ class JobOffer extends Model
 
     public function responsibilities(): HasMany {
         return $this->hasMany(Responsibility::class);
+    }
+
+    public function scopeJobOffersAtLevel(Builder $query, JobLevels $level): void {
+        $query->where('required_level', $level);
+    }
+
+    public function scopeValidJobOffers(Builder $query): void {
+        $query->whereDate('valid_until', '>=', now());
     }
 }
